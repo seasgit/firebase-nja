8ee46dc996412d264dfba3ae9234b424191c56d0
seasone <contact.seasone@gmail.com>
2021-12-16 13:54:40 +0100

    collecte de données depuis l'app font

--- readme.md
+++ readme.md
@@ -1,20 +1,21 @@
-# Firebase tutorial 2
+# Firebase tutorial 3
 
 ## Ressources
 ### voir chapitres dans branches
 https://github.com/iamshaunjp/Getting-Started-with-Firebase-9
 
-## Firebase nouveau projet
-- Avoir un compte firebase
-- Création du projet `FB9nja`
-- Désactivation Google Analytics
-- Sélection web app __</>__ . 
-- Nom de l'app : `ninjapp` _(Firehosting non activé.)_
-### Accéder à la console
-- Sélection l'application et cliquer sur l'icone engrenage
-### Installation et configuration du SDK
-```bash
-npm i firebase
-```
-- Copier le code de l'onglet npm
-- Coller dans `src/index.js`
+## Firestore database
+- créer une database
+- Démarrer en mode test
+- zone Europe
+## Commencer une collection
+- Répéter 3 fois
+    - Ajouter un document. 
+    - Ajout de paires champ/valeurs.
+
+
+![image](./captures/firebase_collection.png)
+
+## dans index.js
+- Connection de l'app à la database
+- Collecte des données et transformation en tableau
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import {
     initializeApp
 } from "firebase/app";
 
+import  {
+    getFirestore,
+    collection,
+    getDocs
+} from 'firebase/firestore'
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyAfRPDxlEi9jbhDcydFdWC_3By8_bP9rag",
@@ -13,4 +19,32 @@ const firebaseConfig = {
     appId: "1:1060641522258:web:e439d267e0898338b8d58b"
 };
 
+// Init firebase app
 const app = initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore(app)
+
+// référence d'une collection
+const colRef =  collection(db, 'books')
+
+// get collection data
+/*
+    NB : doc donne 2 types d'information qui nous inté<rressent.
+    1. l'id du doc
+    2. les données
+
+    On utilise le spread operator pour concaténer dans un objet l'objet data et la clé/valeur id
+
+*/
+getDocs(colRef)
+    .then((snapshots) => {
+        let books = []
+        snapshots.docs.forEach( doc => {
+            books.push({...doc.data(), id:doc.id});
+        })
+        console.log(books);
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
